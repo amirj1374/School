2a06fb6089879b3953fee2e2493e6c4a6f6acaff
amir <[p>
2020-06-29 15:57:35 +0430

    start

---
+++ src/app/students/students.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {Student} from '../../models/student';
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit {
+  student: Student;
+
+  constructor() {
+    this.student = {
+      id: 12,
+      name: 'علی',
+      grade: 20,
+      enName: 'ali'
+    };
+  }
+
+  ngOnInit(): void {
+    const self = this;
+    setTimeout(() => {
+      self.student.name = 'رضا';
+    }, 1000);
+  }
+}
---
+++ src/models/student.ts
@@ -0,0 +1,6 @@
+export class Student {
+  id: number;
+  name: string;
+  grade: number;
+  enName: string;
+}
